b68b341af21cd144d6796694b050ba9f86a317a7
billyxue <xue.billy@gmail.com>
2024-02-04 15:08:03 +0800

    add commands and subcoammnds

---
+++ shelter_main/src/commands/hello.rs
@@ -0,0 +1,29 @@
+use clap::{ArgMatches, Command};
+
+/*
+pub fn configure(command: Command) -> Command {
+    command.subcommand(Command::new("hello").about("Hello World!"))
+}
+
+// handle 方法接受返回的参数匹配clap 并检查我们的hello子命令是否被调用。
+// 如果是这种情况，它会打印Hello World!到控制台。
+pub fn handle(matches: &ArgMatches) -> anyhow::Result<()> {
+    if let Some(_matches) = matches.subcommand_matches("hello") {
+        println!("Hello World!");
+    }
+
+    Ok(())
+}
+*/
+
+pub fn configure() -> Command {
+    Command::new("hello").about("Hello World!")
+}
+
+pub fn handle(matches: &ArgMatches) -> anyhow::Result<()> {
+    if let Some(_matches) = matches.subcommand_matches("hello") {
+        println!("Hello World!");
+    }
+
+    Ok(())
+}
---
+++ shelter_main/src/commands/mod.rs
@@ -0,0 +1,35 @@
+/*
+use clap::{ArgMatches, Command};
+
+pub fn configure(command: Command) -> Command {
+    command.subcommand(Command::new("hello").about("Hello World!"))
+}
+
+// handle 方法接受返回的参数匹配clap 并检查我们的hello子命令是否被调用。
+// 如果是这种情况，它会打印Hello World!到控制台。
+pub fn handle(matches: &ArgMatches) -> anyhow::Result<()> {
+    if let Some(_matches) = matches.subcommand_matches("hello") {
+        println!("Hello World!");
+    }
+
+    Ok(())
+}
+*/
+
+mod hello;
+mod serve;
+
+use clap::{ArgMatches, Command};
+
+pub fn configure(command: Command) -> Command {
+    command
+        .subcommand(hello::configure())
+        .subcommand(serve::configure())
+}
+
+pub fn handle(matches: &ArgMatches) -> anyhow::Result<()> {
+    hello::handle(matches)?;
+    serve::handle(matches)?;
+
+    Ok(())
+}
